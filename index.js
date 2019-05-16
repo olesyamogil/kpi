346f5106dd3d0a8e888d8a284db2ce615f4b03da
olesy <olesya.mogil@gmail.com>
2019-05-16 09:36:09 +0300

    eslint fix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-'use strict'
 
-var http = require('http')
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.write(req.url === '/' ? 'No request' : 'Request: ' + req.url)
-    res.end()
-}).listen(8080)
+
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(req.url === '/' ? 'No request' : `Request: ${req.url}`);
+  res.end();
+}).listen(8080);
