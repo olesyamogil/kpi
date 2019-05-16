a9ceb9c8c487d60e5db435f6c5db93b2501a435e
olesy <olesya.mogil@gmail.com>
2019-05-16 09:32:59 +0300

    server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+'use strict'
 
+var http = require('http')
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(req.url === '/' ? 'No request' : 'Request: ' + req.url)
+    res.end()
+}).listen(8080)
